0e822ac9cbc1e0c68c80a6354462ba11dea9c987
Roy <redroy@Roys-MacBook-Pro.local>
2020-01-27 17:32:11 -0500

    inicio 2

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import Badge from './component/Badge'
+
+const container = document.getElementById('root')
+
+ReactDOM.render(<Badge />, container);
